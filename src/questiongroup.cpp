4e05a61c2bd7f5778845633193e96143c8abef1a
Richard Rondu <rondu.richard@gmail.com>
2019-03-19 17:22:47 +0100

    Implement QuestionGroup.

--- src/questiongroup.cpp
+++ src/questiongroup.cpp
@@ -4,3 +4,50 @@ QuestionGroup::QuestionGroup(QObject *parent) : QObject(parent)
 {
 
 }
+
+bool QuestionGroup::insertQuestion(Question *q)
+{
+    if(!q){
+        qDebug() << "Could not append Question to QuestionGroup, nullptr!";
+        return false;
+    }
+
+    m_questionsMap.insert(q->id(), q);
+    return true;
+}
+
+bool QuestionGroup::removeQuestion(Question *q)
+{
+    if(!q){
+        qDebug() << "Could not remove Question from QuestionGroup, nullptr!";
+        return false;
+    }
+
+    if(m_questionsMap.contains(q->id())){
+        m_questionsMap.remove(q->id());
+        return true;
+    }else{
+        qDebug() << "Could not remove Question from QuestionGroup, it doesn't exist already.";
+        return false;
+    }
+}
+
+bool QuestionGroup::removeQuestion(QString id)
+{
+    if(m_questionsMap.contains(id)){
+        m_questionsMap.remove(id);
+        return true;
+    }else{
+        qDebug() << "Could not remove Question from QuestionGroup, it doesn't exist already.";
+        return false;
+    }
+}
+
+void QuestionGroup::setName(QString name)
+{
+    if (m_name == name)
+        return;
+
+    m_name = name;
+    emit nameChanged(m_name);
+}
--- src/questiongroup.h
+++ src/questiongroup.h
@@ -2,16 +2,34 @@
 #define QUESTIONGROUP_H
 
 #include <QObject>
+#include <QString>
+#include <QDebug>
+#include <QMap>
+
+#include "question.h"
 
 class QuestionGroup : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
 public:
     explicit QuestionGroup(QObject *parent = nullptr);
 
+    QString name() const { return m_name; }
+
+    bool insertQuestion(Question * q);
+    bool removeQuestion(Question * q);
+    bool removeQuestion(QString id);
+
 signals:
+    void nameChanged(QString name);
 
 public slots:
+    void setName(QString name);
+
+private:
+    QMap<QString, Question *> m_questionsMap;
+    QString m_name;
 };
 
 #endif // QUESTIONGROUP_H
